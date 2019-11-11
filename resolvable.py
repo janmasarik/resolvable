91267d91a6e60337685a6a920efe19e0be147c16
Jan Masarik <cambion9999@gmail.com>
2019-11-11 14:13:57 +0100

    Remove resolving

--- resolvable.py
+++ resolvable.py
@@ -2,93 +2,114 @@ import sys
 import contextlib
 import random
 import string
+import traceback
 import dns.resolver
 
 from socket import gethostbyname
+from collections import namedtuple, defaultdict
+from functools import lru_cache
 from concurrent.futures import ThreadPoolExecutor
 
 resolvable = []
 domains = set()
-possible_wildcards = set()
+wildcard_domains = {}
+DNSResponse = namedtuple("DNSResponse", ["type", "response"])
+DNSRecord = namedtuple("DNSRecord", ["domain", "type", "response"])
+
+# TODO WILDCARD CNAME
+# TODO smart wildcard detection
 
 
-def task(domain):
+def query(domain):
+    response = {"CNAME": "", "AAAA": "", "A": ""}
     with contextlib.suppress(Exception):
         answers = dns.resolver.query(domain, "CNAME")
-        resolvable.append(f"{domain},{str(answers[0].target)[:-1]}")
-        return
+        response["CNAME"] = [str(answers[0].target)[:-1]]
 
     with contextlib.suppress(Exception):
-        ip = gethostbyname(domain)
-        resolvable.append(f"{domain},{ip}")
+        answers = dns.resolver.query(domain, "AAAA")
+        response["AAAA"] = sorted(str(a) for a in answers)
+
+    with contextlib.suppress(Exception):
+        answers = dns.resolver.query(domain, "A")
+        response["A"] = sorted(str(a) for a in answers)
+
+    return response
 
 
-def is_wildcard(domain):
-    try:
-        for sub in (
-            "".join(random.choices(string.ascii_lowercase, k=10)),
-            "plznoresults",
-            "gibbountyplz",
-        ):
-            print(f"Trying {domain}")
-            _ = gethostbyname(f"{sub}.{domain}")
-    except Exception:
-        return False
-    else:
-        return True
+def dns_record_sort(dns_record):
+    return (
+        dns_record.type != "CNAME",
+        dns_record.response,
+        dns_record.domain.count("."),
+        dns_record.domain,
+    )
 
 
 def domain_sort(domain):
-    return (not domain.startswith("wildcard."), domain.count("."), domain)
+    return (domain.count("."), domain)
 
 
-# load domains from all sources into domains set
-for file_name in sys.argv[3:]:
-    with open(file_name) as domains_file:
-        for line in domains_file:
-            domain = line.strip().lower()
-            if not domain:
-                continue
-
-            # Fix domains startnig with .
-            if domain[0] == ".":
-                domain = domain[1:]
-
-            domains.add(domain)
-            domain_by_dots = domain.split(".")
-
-            for i in reversed(range(1, len(domain_by_dots) - 1)):
-                maybe_wildcard = ".".join(domain_by_dots[i:])
-                if maybe_wildcard not in possible_wildcards:
-                    possible_wildcards.add(maybe_wildcard)
+@lru_cache(maxsize=13370)
+def get_wildcard_domain(domain):
+    return query(f"{''.join(random.choices(string.ascii_lowercase, k=18))}.{domain}")
 
 
-possible_wildcards = sorted(possible_wildcards, key=lambda x: x.count("."))
-# print("Possible wildcards: ", possible_wildcards)
+grouped_domains = defaultdict(list)
+# load domains from massdns simple output
+with open(sys.argv[3]) as domains_file:
+    for line in domains_file:
+        # kiwi.com. A 54.192.50.12
+        # kiwi.com. A 54.192.50.75
+        # kiwi.com. A 54.192.50.172
+        # kiwi.com. A 54.192.50.107
+        # kkkkkkk.skypicker.com. CNAME skypicker.com.
+        # www.kiwi.com. CNAME www.kiwi.com.cdn.cloudflare.net.
+        parsed_line = line.strip().lower().split(" ")
+        if not parsed_line:
+            continue
 
-wildcards = set()
-# Filter out static DNS wildcards
-for maybe_wildcard in possible_wildcards:
-    if any(maybe_wildcard.endswith(w) for w in wildcards) or not is_wildcard(
-        maybe_wildcard
-    ):
-        continue
+        parsed_line[0] = parsed_line[0][:-1]
 
-    orig_len = len(domains)
-    wildcards.add(maybe_wildcard)
-    domains = set(d for d in domains if not d.endswith(f".{maybe_wildcard}"))
-    if orig_len != len(domains):
-        domains.add(f"wildcard.{maybe_wildcard}")
+        if parsed_line[1] == "cname" and parsed_line[2][-1] == ".":
+            parsed_line[2] = parsed_line[2][:-1]
 
-with ThreadPoolExecutor() as executor:
-    for domain in domains:
-        future = executor.submit(task, (domain))
+        grouped_domains[parsed_line[0]].append(
+            DNSResponse(parsed_line[1], parsed_line[2])
+        )
+
+for domain, dns_responses in grouped_domains.items():
+    if dns_responses[0].type == "cname":
+        dns_record = DNSRecord(domain, "CNAME", tuple([dns_responses[0].response]))
+    elif dns_responses[0].type == "a":
+        dns_record = DNSRecord(domain, "A", tuple(sorted(a[1] for a in dns_responses)))
+    else:
+        print(domain, dns_responses)
+        raise RuntimeError("Unexpected DNS response!")
+
+    domain_by_dots = dns_record.domain.split(".")
+
+    for i in reversed(range(1, len(domain_by_dots) - 1)):
+        maybe_wildcard = ".".join(domain_by_dots[i:])
+        wildcard_response = get_wildcard_domain(maybe_wildcard)
+        is_wildcard = False
+        for wildcard_ip in wildcard_response[dns_record.type]:
+            for ip in dns_record.response:
+                if ip == wildcard_ip:
+                    is_wildcard = True
+                    break
+
+        if is_wildcard:
+            break
+
+    else:
+        resolvable.append(dns_record)
 
 with open(sys.argv[1], "w") as out:
-    out.write("\n".join(sorted(resolvable, key=domain_sort)))
+    for d in sorted(resolvable, key=dns_record_sort):
+        responses_string = ", ".join(d.response)
+        out.write(f"{d.domain} {d.type} {responses_string}\n")
 
-# Replace resolvable with domains for more results (but more false positives)
 with open(sys.argv[2], "w") as out_domains:
-    out_domains.write(
-        "\n".join((sorted((r.split(",")[0] for r in resolvable), key=domain_sort)))
-    )
+    for dom in sorted((d.domain for d in resolvable), key=domain_sort):
+        out_domains.write(f"{dom}\n")
